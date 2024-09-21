7ff4775faa0f4ab6c43d23ae03e8f8c3f5629cce
sahil <sahil@gmail.com>
2024-09-21 18:10:26 +0530

    fixed map

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const flash=require('connect-flash')
 const passport=require('passport')
 const LocalStrategy=require('passport-local')
 const mongoSanitize=require('express-mongo-sanitize')
-const helmet=require('helmet')
 const MongoStore = require('connect-mongo');
 
 
@@ -25,7 +24,7 @@ const userRoutes=require('./routes/users')
 const campgroundRoutes=require('./routes/campgrounds.js')
 const reviewRoutes=require('./routes/reviews')
 
-const dbUrl='mongodb://127.0.0.1:27017/yelp-camp' 
+const dbUrl='mongodb://127.0.0.1:27017/yelp-camp'
 
 main().catch(err => console.log(err));
 
@@ -46,9 +45,6 @@ app.use(mongoSanitize({
     replaceWith:'_'
 }))//removes $ sign prefixed queries from req.query
 
-// store.on("error",function(e){
-//     console.log("SESSION STORE ERROR",e)
-// })
 
 const sessionConfig={
     store:MongoStore.create({
@@ -62,7 +58,7 @@ const sessionConfig={
     saveUninitialized:true,
     cookie:{
         httpOnly:true,
-        //the line below means only people r only accesing the site over http (applied after deployment)
+        //the line below means only people r only accessing the site over http (applied after deployment)
         // secure:true,
         expires: Date.now()+1000*60*60*24*7, //millisecs
         maxAge:1000*60*60*24*7
@@ -70,51 +66,6 @@ const sessionConfig={
 };
 app.use(session(sessionConfig));
 app.use(flash());
-// app.use(helmet());
-
-// const scriptSrcUrls = [
-//     "https://stackpath.bootstrapcdn.com/",
-//     "https://api.tiles.mapbox.com/",
-//     "https://api.mapbox.com/",
-//     "https://kit.fontawesome.com/",
-//     "https://cdnjs.cloudflare.com/",
-//     "https://cdn.jsdelivr.net",
-// ];
-// const styleSrcUrls = [
-//     "https://kit-free.fontawesome.com/",
-//     "https://stackpath.bootstrapcdn.com/",
-//     "https://api.mapbox.com/",
-//     "https://api.tiles.mapbox.com/",
-//     "https://fonts.googleapis.com/",
-//     "https://use.fontawesome.com/",
-// ];
-// const connectSrcUrls = [
-//     "https://api.mapbox.com/",
-//     "https://a.tiles.mapbox.com/",
-//     "https://b.tiles.mapbox.com/",
-//     "https://events.mapbox.com/",
-// ];
-// const fontSrcUrls = [];
-// app.use(
-//     helmet.contentSecurityPolicy({
-//         directives: {
-//             defaultSrc: [],
-//             connectSrc: ["'self'", ...connectSrcUrls],
-//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-//             workerSrc: ["'self'", "blob:"],
-//             objectSrc: [],
-//             imgSrc: [
-//                 "'self'",
-//                 "blob:",
-//                 "data:",
-//                 "https://res.cloudinary.com/diwltkauh/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
-//                 "https://images.unsplash.com/",
-//             ],
-//             fontSrc: ["'self'", ...fontSrcUrls],
-//         },
-//     })
-// );
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -151,6 +102,6 @@ app.use((err,req,res,next)=>{
     res.status(status).render('error',{err});
 })
 
-app.listen(3000,()=>{
-    console.log('Serving on port 3000')
+app.listen(5000,()=>{
+    console.log('Serving on port 5000')
 })
--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -2,18 +2,18 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/light-v10", // style URL
-  center: campground.geometry.coordinates, // starting position [lng, lat]
+  center: campground.features.geometry.coordinates, // starting position [lng, lat]
   zoom: 10, // starting zoom
 });
 
 map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
-    .setLngLat(campground.geometry.coordinates)
+    .setLngLat(campground.features.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({offset:25})
         .setHTML(
-            `<h4>${campground.title}</h4><p>${campground.location}</p>`
+            `<h4>${campground.features.title}</h4><p>${campground.features.location}</p>`
         )
     )
     .addTo(map)
